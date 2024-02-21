15e28ad7f9e57d4a9f7fe42e03bacdf28f47a07d
Trugamr <trugamr@live.in>
2024-02-21 23:01:47 +0530

    feat: add flag to rewrite not-found to index

--- cmd/root.go
+++ cmd/root.go
@@ -5,12 +5,14 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rootCmd.Flags().StringP("listen", "l", ":8080", "Address to listen for incoming requests")
+	rootCmd.Flags().BoolP("single", "s", false, "Rewrite not-found requests to index.html")
 }
 
 var rootCmd = &cobra.Command{
@@ -21,6 +23,7 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// Collect flags and arguments
 		listen := cmd.Flag("listen").Value.String()
+		single, _ := cmd.Flags().GetBool("single")
 
 		// If no path is provided, use the current directory
 		path := "."
@@ -35,12 +38,23 @@ var rootCmd = &cobra.Command{
 		}
 
 		// Create file server and serve files
-		dir := http.Dir(path)
-		server := http.FileServer(dir)
+		fs := http.FileServer(http.Dir(path))
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if single {
+				// If required file is not found or is a directory, rewrite to index.html
+				info, err := os.Stat(filepath.Join(path, r.URL.Path))
+				if (err != nil && os.IsNotExist(err)) || (err == nil && info.IsDir()) {
+					r.URL.Path = "/"
+				}
+			}
+
+			fs.ServeHTTP(w, r)
+		})
 
 		fmt.Println("Listening on", listener.Addr().String())
 
-		err = http.Serve(listener, server)
+		err = http.Serve(listener, nil)
 		if err != nil {
 			cobra.CheckErr(err)
 		}
